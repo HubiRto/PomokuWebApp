f48392d7c3f3f2d5958f758116de9fb2ce40468e
hubert <hubert.rybicki.hr@gmail.com>
2023-07-03 03:14:45 +0200

    Dodanie wymogu uwierzetylnienia przed przejściem na główną stronę

--- vue-frontend/PomokuWebApp/src/router/index.js
+++ vue-frontend/PomokuWebApp/src/router/index.js
@@ -7,21 +7,37 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'Home',
+      component: Home,
+      meta: { requiresAuth: true }
     },
     {
       path: "/login",
       name: "login",
-      component: Login
+      component: Login,
+      meta: { requiresAuth: false }
     },
     {
       path: "/register",
       name: "register",
-      component: Register
+      component: Register,
+      meta: { requiresAuth: false }
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const token = localStorage.getItem('token');
+    if (token) {
+      next();
+    } else {
+      next('/login');
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
